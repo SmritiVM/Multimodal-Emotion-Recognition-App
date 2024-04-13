83d41efe8f89116829b5dcbd524529fd6b8e9b02
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-04-13 23:05:05 +0530

    add separate route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import EmotionRecognition from "./components/EmotionRecognition";
 // import DemoCam from "./components/DemoCam";
 
 function App() {
   return (
     <div className="App">
-      <EmotionRecognition/>
+      <Router>
+        <Routes>
+          <Route path = "/fer" element = {<EmotionRecognition/>}/>
+        </Routes>
+      </Router>
+      
       {/* <DemoCam/> */}
     </div>
   );
