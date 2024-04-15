f9f3cb44d276d1d6ea04c83f1d7f1506c30d8f0d
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-04-16 01:01:38 +0530

    Enable video on click

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import EmotionRecognition from "./EmotionRecognition";
 import Chatbot from "./Chatbot";
 
@@ -5,11 +7,25 @@ import Background from "../assets/Background.jpg";
 
 import "./style.css";
 function Home(){
+    const [enableVideo, setEnableVideo] = useState(false);
+
+    const handleVideo = () => {
+        setEnableVideo(true);
+    }
+
     return(
         <div className="container" style = {{"backgroundImage": `url(${Background})`}}>
+            {enableVideo && (
             <div className="web-camera">
                 <EmotionRecognition/>
             </div>
+            )}
+            {!enableVideo && (
+                <div className="no-video">
+                    <button onClick={handleVideo}>Enable Video</button>
+                </div>
+
+            )}
             <div className="chatbot">
                 <Chatbot/>
             </div>
